e5754ef773bccc3931a1e94f91aad23a7e9ad568
ultimateform <heliojmsemedo@gmail.com>
2024-06-02 00:39:41 +0100

    small fix

--- persistent_titles.py
+++ persistent_titles.py
@@ -3,13 +3,11 @@ import asyncio
 import json
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 from reactivex import Observable
 from data import Config, load_config, save_config
 from rcon_listener import RconListener
 from login_observer import LoginObserver
 
-load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
